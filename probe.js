eb2056735a4545972876affdebdee3052985e034
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2022-12-15 20:15:27 -0300

    Create movement methods

--- probe.js
+++ probe.js
@@ -5,7 +5,57 @@ class Probe {
     this.direction = direction;
   }
 
-  
-}
+  // Turns the probe 90 degrees to the left
+  turnLeft() {
+    switch (this.direction) {
+      case 'N':
+        this.direction = 'W';
+        break;
+      case 'W':
+        this.direction = 'S';
+        break;
+      case 'S':
+        this.direction = 'E';
+        break;
+      case 'E':
+        this.direction = 'N';
+        break;
+    }
+  }
 
-module.exports = Probe;
+  // Turns the probe 90 degrees to the right
+  turnRight() {
+    switch (this.direction) {
+      case 'N':
+        this.direction = 'E';
+        break;
+      case 'E':
+        this.direction = 'S';
+        break;
+      case 'S':
+        this.direction = 'W';
+        break;
+      case 'W':
+        this.direction = 'N';
+        break;
+    }
+  }
+
+  // Moves the probe forward in the direction it is facing
+  moveForward() {
+    switch (this.direction) {
+      case 'N':
+        this.position.y += 1;
+        break;
+      case 'E':
+        this.position.x += 1;
+        break;
+      case 'S':
+        this.position.y -= 1;
+        break;
+      case 'W':
+        this.position.x -= 1;
+        break;
+    }
+  }
+}
