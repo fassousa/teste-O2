f7ee5592f6da03d4125af072b3af92e5aad9336b
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2022-12-16 20:07:51 -0300

    Create test file and add assertions

---
+++ probe.test.js
@@ -0,0 +1,18 @@
+// Probe.test.js
+const { Probe } = require('./probe');
+
+describe('Probe', () => {
+  it('should move forward in the direction it is facing', () => {
+  });
+
+  it('should turn left and right', () => {
+  });
+
+  it('should execute a string of commands', () => {
+  });
+
+  it('should throw an error for invalid commands', () => {
+  });
+
+  it('should execute a string of commands for 2 pods', () => {
+});
