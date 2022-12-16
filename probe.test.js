38216cee8cc64772852c4b6336a4886aa9332db9
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2022-12-16 20:10:41 -0300

    Fix import and add test

--- probe.test.js
+++ probe.test.js
@@ -1,8 +1,23 @@
 // Probe.test.js
-const { Probe } = require('./probe');
+const Probe = require('./probe');
 
 describe('Probe', () => {
   it('should move forward in the direction it is facing', () => {
+    const SpaceProbe = new Probe({x: 0, y: 0}, 'N');
+    SpaceProbe.moveForward();
+    expect(SpaceProbe.position).toEqual({x: 0, y: 1});
+
+    SpaceProbe.direction = 'E';
+    SpaceProbe.moveForward();
+    expect(SpaceProbe.position).toEqual({x: 1, y: 1});
+
+    SpaceProbe.direction = 'S';
+    SpaceProbe.moveForward();
+    expect(SpaceProbe.position).toEqual({x: 1, y: 0});
+
+    SpaceProbe.direction = 'W';
+    SpaceProbe.moveForward();
+    expect(SpaceProbe.position).toEqual({x: 0, y: 0});
   });
 
   it('should turn left and right', () => {
@@ -15,4 +30,5 @@ describe('Probe', () => {
   });
 
   it('should execute a string of commands for 2 pods', () => {
+  });
 });
